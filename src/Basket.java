032595c14ccd0238d535a19c98de0de3f528353d
kouloulias <66220093+kouloulias@users.noreply.github.com>
2020-06-09 22:27:31 +0300

    Add files via upload

--- src/Basket.java
+++ src/Basket.java
@@ -14,44 +14,43 @@ import java.util.stream.Collectors;
 import static CarnivAPP.Extras.Utils.iterateSimultaneously;
 
 public class Basket implements ProductStorage {
-    private HashMap<Product, Integer> Products;
+    private HashMap<Product, Integer> products;
     private StringFormatter stringFormatter;
 
     public Basket() {
-        this.Products = new HashMap<>();
+        this.products = new HashMap<>();
         this.stringFormatter = () -> {
-            final int basketSize = this.Products.size();
+            final int basketSize = this.products.size();
             final String itemString = basketSize > 1 ? basketSize + " Προϊόντα." : basketSize + " Προϊόν.";
             return String.format("Το καλάθι έχει %s", itemString);
         };
     }
 
-    public void addProducts(final Product product, final String Size, final int Amount) throws BasketException {
+    public void addProducts(final Product product, final int Amount) throws BasketException {
         if (product != null) {
-            final int currentAmount = this.Products.getOrDefault(product, 0);
-            this.Products.put(product, currentAmount + Amount);
+            final int currentAmount = this.products.getOrDefault(product, 0);
+            this.products.put(product, currentAmount + Amount);
         } else {
             throw new BasketException("Δεν τοποθετήθηκαν προϊόντα στο καλάθι");
         }
     } 
 
-    public void removeProducts(final Product product, final String Size, final int Amount) throws BasketException {
-        if (this.Products.get(product) > Amount) {
-            this.Products.replace(product, this.Products.get(product) - Amount);
-        } else if (this.Products.get(product) == Amount) {
-            this.Products.replace(product, 0);
+    public void removeProducts(final Product product, final int Amount) throws BasketException {
+        if (this.products.get(product) > Amount) {
+            this.products.replace(product, this.products.get(product) - Amount);
+        } else if (this.products.get(product) == Amount) {
+            this.products.replace(product, 0);
         } else {
-            throw new BasketException(String.format("Δεν μπορούν να αφαιρεθούν %d παρουσίες του προϊόντος, διότι υπάρχουν μόνο %d παρουσίες!",
-                    Amount, this.Products.get(product)));
+            throw new BasketException(String.format("Δεν μπορούν να αφαιρεθούν %d παρουσίες του προϊόντος, διότι υπάρχουν μόνο %d παρουσίες!", Amount, this.products.get(product)));
         }
     }
 
     public HashMap<Product, Integer> getProducts() {
-        return Products;
+        return products;
     }
 
     public double calculateTotal() {
-        return this.Products.entrySet().
+        return this.products.entrySet().
                 parallelStream().
                 mapToDouble(product -> product.getKey().getPrice() * product.getValue()).
                 sum();
@@ -64,29 +63,23 @@ public class Basket implements ProductStorage {
     public ArrayList<String> toDBFormat() {
        
         final ArrayList<String> result = new ArrayList<>();
-        final String Names = this.Products.entrySet().
+        final String Names = this.products.entrySet().
                 parallelStream().
                 map(p -> p.getKey().getName()).
                 collect(Collectors.joining(","));
-        final String Sizes = this.Products.entrySet().
-                parallelStream().
-                map(p -> p.getKey().getSize()).
-                collect(Collectors.joining(","));
-        final String Amounts = this.Products.entrySet().
+        final String Amounts = this.products.entrySet().
                 parallelStream().
                 map(p -> p.getValue().toString()).
                 collect(Collectors.joining(","));
         result.add(Names);
-        result.add(Sizes);
         result.add(Amounts);
 
         return result;
     }
 
     @Deprecated
-    public void restoreFromDB(final String productsName, /*final String productsSize,*/ final String productsAmount) {
+    public void restoreFromDB(final String productsName, final String productsAmount) {
         final List<String> Names = Arrays.asList(productsName.split(","));
-        //final List<String> Sizes = Arrays.asList(productsSize.split(","));
         final List<String> Amounts = Arrays.asList(productsAmount.split(","));
 
         iterateSimultaneously(Names, Amounts, (String Name, String Amount) -> {
@@ -102,5 +95,4 @@ public class Basket implements ProductStorage {
     public String toString() {
         return stringFormatter.formatToString();
     }		
-	}
 }
